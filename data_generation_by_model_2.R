3857e429ea677b249d9d5917bdb3323706f5015b
Richard Podkolinski <devlar@gmail.com>
2016-05-16 16:21:30 +0200

    Added Attempt 5 and added weibull explore script.

--- data_generation_by_model_2.R
+++ data_generation_by_model_2.R
@@ -1,5 +1,5 @@
 ## Data Generation By Model
-
+library(rstan)
 
 N = 100
 shape = 1   
@@ -61,7 +61,10 @@ t_obs ~ weibull(shape, scale);
 }
 "
 
-m2f = stan(model_code = m2, data = lfe, cores = 3, chains = 2, iter = 1e5, warmup = 1e3, control=list(adapt_delta=0.99))
+m2f = stan(model_code = m2, data = lfe, 
+           verbose = TRUE,
+           cores = 3, chains = 2, iter = 1e5, warmup = 1e3, 
+           control=list(adapt_delta=0.99))
 print(m2f)
 
 
@@ -137,7 +140,7 @@ w_scale  = beta0 + beta1 * x1 + beta2 * x2
 t_obs = rweibull(N, shape = w_shape, scale = exp(-w_scale))
 ## Make sure this is greater than 0 for all values, call error otherwise
 
-nl_t_obs = -log(t_obs)
+# nl_t_obs = -log(t_obs)
 
 lfe = list(N = N, t_obs = t_obs, x1 = x1, x2 = x2)
 
@@ -158,14 +161,15 @@ real beta2;
 }
 
 model {
+vector[N] scale;
 shape ~ cauchy(0,2);
 beta0 ~ cauchy(0,2);
 beta1 ~ cauchy(0,2);
 beta2 ~ cauchy(0,2);
 for(i in 1:N){
-t_obs[i] ~ weibull(shape, exp( -(beta0 + beta1 * x1[i] + beta2 * x2[i] )));
+  scale[i] <- exp( -(beta0 + beta1 * x1[i] + beta2 * x2[i] ));
 }
-
+t_obs ~ weibull(shape, scale);
 }
 "
 
@@ -174,3 +178,67 @@ print(m4f)
 
 
 plot(density(-log(t_obs)))
+
+
+
+#### Attempt 5 ####
+# Linear Model with Integrated Log
+
+N = 1000
+w_shape = 6
+# replace with Linear Model exp(- (beta_0 + beta_1 x_1 + ...) )
+beta0 = 300 # Average Number of Days until Failure
+beta1 = -10  # Average Number of Days Decrease per Alarm Count
+x1 = rpois(N, 10) # Set the average number of alarms
+
+
+
+
+# t_avgfail = rnorm(N, 300, 1.5)
+w_scale  = beta0 + beta1 * log(x1)
+w_scale  = beta0 + beta1 * x1
+
+t_obs = rweibull(N, shape = w_shape, scale = exp(-w_scale))
+## Make sure this is greater than 0 for all values, call error otherwise
+
+# nl_t_obs = log(t_obs)
+
+lfe = list(N = N, t_obs = t_obs, x1 = x1 )
+
+m5 = "
+data {
+int<lower=0>  N;
+real<lower=0> t_obs[N];
+int x1[N];
+int x2[N];
+}
+
+parameters {
+real<lower=0> shape;
+//real<lower=0> scale;
+real beta0;
+real beta1;
+real beta2;
+}
+
+model {
+vector[N] scale;
+shape ~ cauchy(0,2);
+beta0 ~ cauchy(0,2);
+beta1 ~ cauchy(0,2);
+beta2 ~ cauchy(0,2);
+for(i in 1:N){
+scale[i] <- exp( -(beta0 + beta1 * x1[i] ));
+}
+t_obs ~ weibull(shape, scale);
+}
+"
+
+m5f = stan(model_code = m5, data = lfe, 
+           cores = 7, chains = 2, iter = 1e4, warmup = 1e3, control=list(adapt_delta=0.99))
+print(m5f)
+
+
+# plot(density(-log(t_obs)))
+
+
---
+++ weibull_explorer.R
@@ -0,0 +1,78 @@
+## Weibull Density Explore
+
+#### Density Plot ####
+t_obs = seq(0, 1000, by = 0.5)
+f05 = dweibull(t_obs, 0.5, 300)
+f10 = dweibull(t_obs, 1, 300)
+f30 = dweibull(t_obs, 3, 300)
+
+
+plot(t_obs, f30, type = "n")
+lines(t_obs, f05, type ="l", col = "red")
+lines(t_obs, f10, type ="l", col = "blue")
+lines(t_obs, f30, type ="l", col = "magenta")
+
+#### Probability Plot ####
+
+p05 = pweibull(t_obs, 0.5, 300)
+p10 = pweibull(t_obs, 1, 300)
+p30 = pweibull(t_obs, 3, 300)
+
+plot(t_obs, p30, type = "n")
+lines(t_obs, p05, type ="l", col = "red")
+lines(t_obs, p10, type ="l", col = "blue")
+lines(t_obs, p30, type ="l", col = "magenta")
+
+#### Quantile Plot ####
+
+p_obs = seq(0,0.999, by = 0.01)
+
+q05 = qweibull(p_obs, 0.5, 300)
+q10 = qweibull(p_obs, 1, 300)
+q30 = qweibull(p_obs, 3, 300)
+
+plot(p_obs, q30, type = "n")
+lines(p_obs, q05, type ="l", col = "red")
+lines(p_obs, q10, type ="l", col = "blue")
+lines(p_obs, q30, type ="l", col = "magenta")
+
+#### Failure Rate ####
+library(eha)
+
+# Hazard
+# res <- ifelse(x < 0, 0, shape * (x/scale)^(shape - 1)/scale)
+
+
+h05 = hweibull(t_obs, 0.5, 300)
+h10 = hweibull(t_obs, 1, 300)
+h30 = hweibull(t_obs, 3, 300)
+
+plot(t_obs, h30, type = "n")
+lines(t_obs, h05, type ="l", col = "red")
+lines(t_obs, h10, type ="l", col = "blue")
+lines(t_obs, h30, type ="l", col = "magenta")
+
+
+#### Properties of the Weibull ####
+
+shape = 6
+scale = 300
+y = 1:5
+loc = 0
+f_x = (shape / scale) * ((y - loc) / scale)^(shape - 1) * exp(-( (y - loc) /scale)^shape)
+# dweibull(y, shape, scale)
+# Where f(y) >= 0,  y >= 0, shape & scale > 0, -Inf < loc < Inf
+
+R_x = exp( -((y - loc)/ scale)^shape )
+#1 - pweibull(1:5, shape, scale)
+
+h_x = (shape/scale) * (y / scale)^(shape - 1)
+# AKA shape * (x/scale)^(shape - 1)/scale
+# hweibull(y, shape, scale)
+
+H_x = (y/scale)^(shape)
+#Hweibull(y, shape, scale)
+
+# Time to Failure
+mean_ttf = loc + scale * gamma( (1 / shape) + 1 ) 
+median_ttf = loc + scale * (log(2))^(1/shape)
