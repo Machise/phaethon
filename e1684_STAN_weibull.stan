a9743156c53e1f0178801e7313412be4d082b6d3
Richard Podkolinski <richard.podkolinski@ikea.com>
2016-05-16 19:38:57 +0200

    Added e1684 weibull model.

---
+++ e1684_STAN_weibull.stan
@@ -0,0 +1,30 @@
+data{
+  int<lower=0> N_uncensored;
+  int<lower=0> N_censored;
+  int<lower=0> trt_uncensored[N_uncensored];
+  int<lower=0> trt_censored[N_censored];
+  real<lower=0> t_censored[N_censored];
+  real<lower=0> t_uncensored[N_uncensored];
+}
+
+parameters{
+  real alpha;
+  real beta0;
+  real beta1;
+  real<lower=1> t2_censored[N_censored]; // t_censored / t_censored
+}
+model{
+  alpha ~ gamma(0.001, 0.001);
+  beta1 ~ normal( 0 , 1e+06 );
+  beta0 ~ normal( 0 , 1e+06 );
+  for( i in 1:N_uncensored ) {
+    // t_uncensored[i] ~ exponential( 1 / exp( -(beta0 + beta1 * trt_uncensored[i])) );
+    t_uncensored[i] ~ weibull(alpha, exp( -(beta0 + beta1 * trt_uncensored[i])) );
+    // t_uncensored[i] ~ weibull(r, exp(-beta[trt_uncensored[i]] / r ));
+  }
+  for( i in 1:N_censored ){
+    // increment_log_prob(weibull_ccdf_log(t_censored[i], 1, exp( -(beta0 + beta1 * trt_censored[i] )) / t_censored[i]));
+    t2_censored[i] ~ weibull(alpha, exp( -(beta0 + beta1 * trt_censored[i] )) / t_censored[i]);
+    // t2_censored[i] ~ weibull(r, exp(-beta[trt_censored[i]] / r) / t_censored[i]);
+  }
+}
---
+++ e1684_comparison_weibull.R
@@ -0,0 +1,56 @@
+library(survival)
+library(runjags)
+library(coda)
+library(rstan)
+library(rethinking)
+source("e1684_data.R")
+
+
+# Litmus Test
+### Survival Package Fit
+m1 = survreg(Surv(obs_t, event) ~ trt, data = dfe, dist = "weibull")
+summary(m1)
+
+
+
+## JAGS Model
+cat("
+    model
+    {
+    # Priors
+    beta0 ~ dnorm(0, 1e-6)
+    beta1 ~ dnorm(0, 1e-6)
+    alpha ~ dgamma(0.001, 0.001)
+    
+    # Likelihood
+    for(i in 1:N) {
+      isCensored[i] ~ dinterval(t[i], t.cen[i])
+      t[i] ~ dweibull(alpha,lambda[i])
+      lambda[i] <- exp(beta0 + beta1 * trt[i])
+      }
+    # Mean time to death
+      
+      #mu <- alpha * loggam(( 1 + (1 / lambda) ))
+
+      median0 <- pow(log(2) * exp(-beta0), 1/alpha)
+			median1 <- pow(log(2) * exp(-beta0-beta1), 1/alpha)
+
+
+    # Predictions
+    #for(t in 1:new_t) {
+    #  S_t[t] <- exp( -(new_t[t] / alpha))^lambda 
+    #  }
+    }
+    ", file = "survival_cancer_weibull.jags"
+)
+
+m2 = run.jags(data = lfe,
+              model = "survival_cancer_weibull.jags",
+              monitor = c("alpha", "beta0", "beta1", "median0", "median1"),
+              sample = 1e5, burnin = 1e3, n.chains = 3)
+print(m2)[,"Mean"]
+summary(m2)
+
+summary(m1)
+
+m4 = stan(file = "e1684_STAN_weibull.stan", data = lfs, iter = 1e4, warmup = 1e3); precis(m4)
